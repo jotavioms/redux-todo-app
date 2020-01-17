97593e65ab860d050856ddf1f4f612659f821ebc
João Souza <j.otavioms@gmail.com>
2020-01-17 14:52:59 -0300

    Create remove action

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import IconButton from '../template/iconButton';
-import { markAsDone, markAsPending } from './todoActions';
+import { markAsDone, markAsPending, remove } from './todoActions';
 
 const TodoList = props => {
   const renderRows = () => {
@@ -28,7 +28,7 @@ const TodoList = props => {
           <IconButton
             style='danger'
             icon='trash-o'
-            onClick={() => props.handleRemove(todo)}
+            onClick={() => props.remove(todo)}
             hide={!todo.done}
           />
         </td>
@@ -52,6 +52,6 @@ const TodoList = props => {
 };
 
 const mapStateToProps = state => ({ list: state.todo.list });
-const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
